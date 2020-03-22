7109492a20c3e86b1d0d460c1c66931af0b81360
shivamg.cr7@gmail.com <shivamg.cr7@gmail.com>
2020-03-23 03:05:36 +0530

    [Shivam] Calculate Wages till a condition of total working hours or days is reached for a month.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,32 +5,37 @@ fullTimeEmp=1
 partTimeEmp=2
 daysPerMonth=20
 totalSalary=0
+totalHr=0
+maximumHr=100
+days=0
 
 calEmpMonthlyWage()
 {
-	for (( i=1; i<=$daysPerMonth; i++  ))
-	do
-		random=$((RANDOM%3))
-		case $random in
-		$fullTimeEmp )
-			empHr=8 ;;
+        while [ $totalHr -le $maximumHr -a $days -le $daysPerMonth ]
+        do
+                random=$((RANDOM%3))
+                case $random in
+                $fullTimeEmp )
+                        empHr=8 ;;
 
-		$partTimeEmp )
-			empHr=4 ;;
+                $partTimeEmp )
+                        empHr=4 ;;
 
-		* )
-			empHr=0 ;;
-		esac
+                * )
+                        empHr=0 ;;
+                esac
 
-		salary=$(( $perHrWage * $empHr ))
-		echo "Employee daily salary is $salary "
-		totalSalary=$(( $salary + $totalSalary ))
-	done
+                salary=$(( $perHrWage * $empHr ))
+                echo "Employee daily salary is $salary "
+                totalSalary=$(( $salary + $totalSalary ))
+                totalHr=$(( $totalHr + $empHr ))
+                ((++days))
+        done
 }
 
 main()
 {
-	calEmpMonthlyWage
+        calEmpMonthlyWage
 }
 
 main
