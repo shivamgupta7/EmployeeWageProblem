554a0d47eeabf2c659c2d6f0bc8f37f110c7ba44
shivamg.cr7@gmail.com <shivamg.cr7@gmail.com>
2020-03-23 01:02:36 +0530

    [Shivam] Calculate Employee Monthly Wage.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,31 +1,37 @@
-#! /bin/bash
+#! /bin/bash -x
 
 perHrWage=20
 fullTimeEmp=1
 partTimeEmp=2
+daysPerMonth=20
+totalSalary=0
 
-checkDailyWageUsingCase()
+calEmpMonthlyWage()
 {
-	Random=$((RANDOM%3))
-	case $Random in
-	$fullTimeEmp )
-		empHr=8
-		echo "Employee is present for full day.";;
-	$partTimeEmp )
-		empHr=4
-		echo "Employee is present for part time.";;
-	* )
-		empHr=0
-		echo "Employee is absent.";;
-	esac
+	for (( i=1; i<=$daysPerMonth; i++  ))
+	do
+		random=$((RANDOM%3))
+		case $random in
+		$fullTimeEmp )
+			empHr=8 ;;
+
+		$partTimeEmp )
+			empHr=4 ;;
+
+		* )
+			empHr=0 ;;
+		esac
+
+		salary=$(( $perHrWage * $empHr ))
+		echo "Employee daily salary is $salary "
+		totalSalary=$(( $salary + $totalSalary ))
+	done
 }
 
 main()
 {
-	checkDailyWageUsingCase
+	calEmpMonthlyWage
 }
 
 main
-
-salary=$(( $perHrWage * $empHr ))
-echo "Employee daily salary is $salary"
+echo "Employee total Salary Per Month is $totalSalary"
