d594c4ba7dbd7a348f1aa03951cb7bb7f9d98385
shivamg.cr7@gmail.com <shivamg.cr7@gmail.com>
2020-03-22 23:45:57 +0530

    [Shivam] Check employee is present or absent.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,20 @@
 #! /bin/bash
 
 echo "Welcome to Employee Wage Computation Program."
+
+checkEmpAttendance()
+{
+	if [ $((RANDOM%2)) -eq 1 ]
+	then
+		echo "Employee is present."
+	else
+		echo "Employee is absent."
+	fi
+}
+
+main()
+{
+	checkEmpAttendance
+}
+
+main
